b716b0a0f4ea68f2e1a54367f6051ed6da943687
valera730 <telmob@yandex.ru>
2025-12-30 10:48:20 +0300

    Add files via upload

---
+++ Test-Driven Development/resources/views/posts/show.blade.php
@@ -0,0 +1,7 @@
+<h3>Show page</h3>
+<div>
+    <div>
+        <h4>{{ $post->title }}</h4>
+        <p>{{ $post->description }}</p>
+    </div>
+</div>
---
+++ Test-Driven Development/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::patch('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
